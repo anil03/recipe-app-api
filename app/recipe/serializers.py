14092ac3d44c05a3d6af429c9fc6044b49119317
anil gupta <anil03gupta03@gmail.com>
2019-08-17 19:03:35 +0530

    final create recipe feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -35,7 +35,11 @@ class RecipeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Recipe
-        fields = ('id', 'tilte', 'ingredients', 'tags', 'time_minutes',
-                  'price', 'link')
+        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes', 'price', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -41,8 +41,33 @@ class RecipeViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes=(IsAuthenticated,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-        """Retreive the recipe foe authenticated user"""
+        """Retrieve the recipe foe authenticated user"""
         return self.queryset.filter(user=self.request.user)
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new Recipe"""
+        serializer.save(user=self.request.user)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
